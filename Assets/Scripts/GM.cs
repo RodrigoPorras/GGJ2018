fe584ac3d41dea33a8af4051873dcd43ed65bb9a
Rodrigo Porras <rodrigo.porrras@udea.edu.co>
2018-01-26 19:39:01 -0500

    gm

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public static GM instance;
+
+	void Awake () {
+		if(instance == null){
+			instance = this;
+		}else{
+			Destroy(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
